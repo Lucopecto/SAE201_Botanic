e10788e0026e646978df409f48443b30d887dd42
sftss <tasdemirsefer2005@gmail.com>
2024-06-04 09:18:29 +0200

    modif classe categorie + caracteristique

--- SAE201_Botanic/Caracteristique.cs
+++ SAE201_Botanic/Caracteristique.cs
@@ -6,7 +6,64 @@ using System.Threading.Tasks;
 
 namespace SAE201_Botanic
 {
-    internal class Caracteristique
+    public class Caracteristique
     {
+        #region Champs
+        private int numCaracteristique;
+        private string nomCaracteristique;
+        #endregion
+
+        #region Propriete
+        public int NumCaracteristique
+        {
+            get
+            {
+                return numCaracteristique;
+            }
+
+            set
+            {
+                numCaracteristique = value;
+            }
+        }
+
+        public string NomCaracteristique
+        {
+            get
+            {
+                return this.nomCaracteristique;
+            }
+
+            set
+            {
+                this.nomCaracteristique = value;
+            }
+        }
+        #endregion
+
+        #region Constructeur
+        public Caracteristique(int numCaracteristique, string nomCaracteristique)
+        {
+            this.NumCaracteristique = numCaracteristique;
+            this.NomCaracteristique = nomCaracteristique;
+        }
+        #endregion
+
+        #region Methode
+        public override bool Equals(object? obj)
+        {
+            return obj is Caracteristique caracteristique &&
+                   this.NumCaracteristique == caracteristique.NumCaracteristique &&
+                   this.NomCaracteristique == caracteristique.NomCaracteristique;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.NumCaracteristique, this.NomCaracteristique);
+        }
+
+
+        #endregion
+
     }
 }
--- SAE201_Botanic/Categorie.cs
+++ SAE201_Botanic/Categorie.cs
@@ -6,7 +6,79 @@ using System.Threading.Tasks;
 
 namespace SAE201_Botanic
 {
-    internal class Categorie
+    public class Categorie
     {
+        #region Champs
+        private int numCategorie;
+        private TypeProduit unTypeProduit;
+        private string libelleCategorie;
+        #endregion
+
+        #region Propriete
+        public int NumCategorie
+        {
+            get
+            {
+                return numCategorie;
+            }
+
+            set
+            {
+                numCategorie = value;
+            }
+        }
+
+        public TypeProduit UnTypeProduit
+        {
+            get
+            {
+                return unTypeProduit;
+            }
+
+            set
+            {
+                unTypeProduit = value;
+            }
+        }
+
+        public string LibelleCategorie
+        {
+            get
+            {
+                return this.libelleCategorie;
+            }
+
+            set
+            {
+                this.libelleCategorie = value;
+            }
+        }
+        #endregion
+
+        #region Constructeur
+        public Categorie(int numCategorie, TypeProduit unTypeProduit, string libelleCategorie)
+        {
+            this.NumCategorie = numCategorie;
+            this.UnTypeProduit = unTypeProduit;
+            this.LibelleCategorie = libelleCategorie;
+        }
+
+        #endregion
+
+        #region Methode
+        public override bool Equals(object? obj)
+        {
+            return obj is Categorie categorie &&
+                   this.NumCategorie == categorie.NumCategorie &&
+                   EqualityComparer<TypeProduit>.Default.Equals(this.UnTypeProduit, categorie.UnTypeProduit) &&
+                   this.LibelleCategorie == categorie.LibelleCategorie;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.NumCategorie, this.UnTypeProduit, this.LibelleCategorie);
+        }
+
+        #endregion
     }
 }
