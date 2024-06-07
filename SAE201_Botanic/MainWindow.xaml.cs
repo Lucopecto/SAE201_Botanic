b432d9aa2e1e6721442cc132aa01b064ee67c074
feyy7435 <feyy.tnstp05@gmail.com>
2024-06-07 11:01:05 +0200

    produit

--- SAE201_Botanic/MainWindow.xaml.cs
+++ SAE201_Botanic/MainWindow.xaml.cs
@@ -58,6 +58,34 @@ namespace SAE201_Botanic
                     MessageBox.Show("Erreur : " + ex, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
+            DataTable lesProduits = appData.Read("SELECT p.numProduit,c.nomCouleur, cat.numCategorie, f.numFournisseur, p.nomProduit, p.tailleProduit, p.descriptionProduit, p.prixVente, p.prixAchat "+
+                "FROM produit p " +
+                "JOIN couleur c ON p.numProduit = c.numCouleur " +
+                "JOIN categorie cat ON p.numµProduit = cat.numCategorie" +
+                "JOIN fournisseur f ON p.numProduit = f.numFournisseur" +
+                "JOIN type_produit tp ON cat.numCategorie = tp.numType");
+            foreach (DataRow unProduit in lesProduits.Rows)
+            {
+                try
+                {
+                    bool codeLocal;
+                    Couleur couleur = new Couleur(unProduit["nomCouleur"].ToString());
+                    TypeProduit typeProduit = new TypeProduit(int.Parse(unProduit["numType"].ToString()), unProduit["nomType"].ToString());
+                    Categorie categorie = new Categorie(int.Parse(unProduit["numCategorie"].ToString()), typeProduit, unProduit["libelleCategorie"].ToString());
+                    if (unProduit["codelocal"].ToString() == "True")
+                         codeLocal = true;
+                    else
+                        codeLocal = false;
+                    Fournisseur fournisseur = new Fournisseur(int.Parse(unProduit["numFournisseur"].ToString()), unProduit["nomFournisseur"].ToString(), codeLocal);
+;                   Produit produit = new Produit(
+                        int.Parse(unProduit["numProduit"].ToString()),couleur, categorie, fournisseur,unProduit["nomProduit"].ToString(), unProduit["tailleProduit"].ToString(), unProduit["descriptionProduit"].ToString(), double.Parse(unProduit["prixVente"].ToString()), double.Parse(unProduit["prixAchat"].ToString()));
+               
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur : " + ex, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
         private void textMotClef_TextChanged(object sender, TextChangedEventArgs e)
