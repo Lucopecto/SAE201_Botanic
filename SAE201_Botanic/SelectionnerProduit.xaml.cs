35600bcf36630e98123de48416ddf435d734d205
feyy7435 <feyy.tnstp05@gmail.com>
2024-06-10 09:24:51 +0200

    aaaaa

--- SAE201_Botanic/SelectionnerProduit.xaml.cs
+++ SAE201_Botanic/SelectionnerProduit.xaml.cs
@@ -22,7 +22,7 @@ namespace SAE201_Botanic
     /// </summary>
     public partial class SelectionnerProduit : Window
     {
-        public List<Produit> CommandeSelect;
+        public CommandeAchat CommandeSelect;
         public ApplicationData data;
         public ObservableCollection<Item> Items { get; set; }
         public ObservableCollection<Produit> LesProduits { get; set; }
@@ -30,44 +30,35 @@ namespace SAE201_Botanic
         public SelectionnerProduit()
         {
             InitializeComponent();
-
-            Items = new ObservableCollection<Item>
-
-            {
-                new Item {  Produit = "Produit 1", Quantity = 15, IsSelected = false },
-
-            };
-
-            this.DataContext = Items;
-            ApplicationData appData = new ApplicationData();
             LesProduits = new ObservableCollection<Produit>();
+            this.DataContext = this;
 
+            ApplicationData appData = new ApplicationData();
             string sql = "SELECT p.numProduit, c.nomCouleur, cat.numCategorie, p.descriptionProduit, cat.libellecategorie, tp.numtype, tp.nomtype, f.numFournisseur, f.nomfournisseur, f.codelocal, p.nomProduit, p.tailleProduit,  p.prixVente, p.prixAchat " +
-                "FROM produit p " +
-                "JOIN couleur c ON p.nomCouleur = c.nomcouleur " +
-                "JOIN categorie cat ON p.numCategorie = cat.numCategorie " +
-                "JOIN fournisseur f ON p.numFournisseur = f.numFournisseur " +
-                "JOIN type_produit tp ON cat.numtype = tp.numtype";
-            Console.WriteLine(sql);
+                         "FROM produit p " +
+                         "JOIN couleur c ON p.nomCouleur = c.nomcouleur " +
+                         "JOIN categorie cat ON p.numCategorie = cat.numCategorie " +
+                         "JOIN fournisseur f ON p.numFournisseur = f.numFournisseur " +
+                         "JOIN type_produit tp ON cat.numtype = tp.numtype";
+
             DataTable lesProduits = appData.Read(sql);
+
             foreach (DataRow unProduit in lesProduits.Rows)
             {
                 try
                 {
-                    bool codeLocal;
+                    bool codeLocal = unProduit["codelocal"].ToString() == "True";
                     Couleur couleur = new Couleur(unProduit["nomCouleur"].ToString());
                     TypeProduit typeProduit = new TypeProduit(int.Parse(unProduit["numType"].ToString()), unProduit["nomType"].ToString());
                     Categorie categorie = new Categorie(int.Parse(unProduit["numCategorie"].ToString()), typeProduit, unProduit["libelleCategorie"].ToString());
-                    if (unProduit["codelocal"].ToString() == "True")
-                        codeLocal = true;
-                    else
-                        codeLocal = false;
                     Fournisseur fournisseur = new Fournisseur(int.Parse(unProduit["numFournisseur"].ToString()), unProduit["nomFournisseur"].ToString(), codeLocal);
-                    ; Produit produit = new Produit(
-                                            int.Parse(unProduit["numProduit"].ToString()), couleur, categorie, fournisseur, unProduit["nomProduit"].ToString(), unProduit["tailleProduit"].ToString(), unProduit["descriptionProduit"].ToString(), double.Parse(unProduit["prixVente"].ToString()), double.Parse(unProduit["prixAchat"].ToString()));
-                    LesProduits.Add(produit);
+                    Produit produit = new Produit(
+                        int.Parse(unProduit["numProduit"].ToString()), couleur, categorie, fournisseur,
+                        unProduit["nomProduit"].ToString(), unProduit["tailleProduit"].ToString(),
+                        unProduit["descriptionProduit"].ToString(), double.Parse(unProduit["prixVente"].ToString()),
+                        double.Parse(unProduit["prixAchat"].ToString()));
 
-                    Console.WriteLine("Chargement de la couleur : " + unProduit["nomCouleur"].ToString());
+                    LesProduits.Add(produit);
                 }
                 catch (Exception ex)
                 {
